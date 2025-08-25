8af2bf73ae56d8a2c97fbc9fb2d7adcb93b39025
lakshmanana456 <elakshmanan10501@gmail.com>
2025-08-25 15:45:51 +0530

    change file extension

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-import SearchProvider from './components/SearchContext.jsx'
-import { CheckoutProvider } from './components/CheckoutContext.jsx'
-import { CartProvider } from './components/CartContext.jsx'
+import App from './App.js'
+import SearchProvider from './components/SearchContext.js'
+import { CheckoutProvider } from './components/CheckoutContext.js'
+import { CartProvider } from './components/CartContext.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
