f8cbdf0881a08ec686f191c3dfefccd1d4d3e984
lakshmanana456 <elakshmanan10501@gmail.com>
2025-08-25 16:26:31 +0530

    neww

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.js'
-import SearchProvider from './components/SearchContext.js'
-import { CheckoutProvider } from './components/CheckoutContext.js'
-import { CartProvider } from './components/CartContext.js'
+import App from './App.jsx'
+import SearchProvider from './components/SearchContext.jsx'
+import { CheckoutProvider } from './components/CheckoutContext.jsx'
+import { CartProvider } from './components/CartContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
